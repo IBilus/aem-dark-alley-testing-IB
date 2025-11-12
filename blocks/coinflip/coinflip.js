2fda7c3c5eb722270a45e2db97ed5008a0470d06
Ivan Bilus <Ivan.Bilus@ibm.com>
2025-11-12 10:44:17 +0100

    fwafwafwafwa

--- blocks/coinflip/coinflip.js
+++ blocks/coinflip/coinflip.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-  block.classList.add(`testClassName`)
+  block.classList.add(`testClassName`);
 
   // setup image columns
   [...block.children].forEach((row) => {
--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,6 +2,7 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add('columns-${cols.length}-cols');
   block.classList.add('testClassName');
+  block.classList.add('testClassName2');
 
   // setup image columns
   [...block.children].forEach((row) => {
