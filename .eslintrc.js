c6e9e9f0a5f9077fd52147610268fc3050f286b1
Ivan Bilus <Ivan.Bilus@ibm.com>
2025-11-17 15:13:09 +0100

    popppp

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,5 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param,
-    'prettier/prettier': ['error', { singleQuote: true }]
   },
 };
