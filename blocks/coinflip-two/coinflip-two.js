a85b3d153088023d44a31144edb9a74f55838917
Ivan Bilus <Ivan.Bilus@ibm.com>
2025-11-17 15:06:50 +0100

    wdadawda

--- blocks/coinflip-two/coinflip-two.js
+++ blocks/coinflip-two/coinflip-two.js
@@ -2,7 +2,6 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
   block.classList.add(`testClassNameCoinflip`);
-  let a = 2;
 
   /*const ul = document.createElement('ul');
 
